0d396aae7ecd10db94dfed0de4ebd3791cee63af
Prescott Hogan <w040395@nscc.ca>
2024-05-07 11:59:16 -0300

    Now multithreaded, movement and LCD updates are now independent and do not interfere with one another

--- motor.py
+++ motor.py
@@ -1,26 +1,31 @@
 import time
 import evdev
 from evdev import InputDevice, ecodes
-from rpi_lcd import LCD
+from RPLCD.i2c import CharLCD
 import RPi.GPIO as GPIO
+import threading
 
 GPIO.setwarnings(False)
 
-lcd = LCD()
+LCD_COLUMNS = 16
+LCD_ROWS = 2
 
-con1ena = 13
-con1in1 = 16
-con1in2 = 19
-con1in3 = 20
-con1in4 = 26
-con1enb = 21
+lcd = CharLCD('PCF8574', 0x27, cols=LCD_COLUMNS, rows=LCD_ROWS)
 
-con2ena = 10
-con2in1 = 4
-con2in2 = 14
-con2in3 = 8
-con2in4 = 11
-con2enb = 25
+con1ena = 10
+con1in1 = 4
+con1in2 = 14
+con1in3 = 8
+con1in4 = 11
+con1enb = 25
+
+con2ena = 13
+con2in1 = 16
+con2in2 = 19
+con2in3 = 20
+con2in4 = 26
+con2enb = 21
+
 
 GPIO.setmode(GPIO.BCM)
 
@@ -35,11 +40,11 @@ GPIO.setup(con1enb,GPIO.OUT)
 GPIO.setup(con2in1,GPIO.OUT)
 GPIO.setup(con2in2,GPIO.OUT)
 GPIO.setup(con2ena,GPIO.OUT)
-
+# 
 GPIO.setup(con2in3,GPIO.OUT)
 GPIO.setup(con2in4,GPIO.OUT)
 GPIO.setup(con2enb,GPIO.OUT)
-
+# 
 GPIO.output(con1in1,GPIO.LOW)
 GPIO.output(con1in2,GPIO.LOW)
 GPIO.output(con1ena,GPIO.LOW)
@@ -65,195 +70,232 @@ back_left_pwm.start(0)
 back_right_pwm = GPIO.PWM(con2enb, 1000)
 back_right_pwm.start(0)
 
+movement_status = "Hello World"
+
 def forward():
-   #front right
-   GPIO.output(con1in1,GPIO.LOW)
-   GPIO.output(con1in2,GPIO.HIGH)
-   
-   #front left
-   GPIO.output(con1in3,GPIO.LOW)
-   GPIO.output(con1in4,GPIO.HIGH)
-   
-   #back left
-   GPIO.output(con2in1,GPIO.LOW)
-   GPIO.output(con2in2,GPIO.HIGH)
-   
-   #back right
-   GPIO.output(con2in3,GPIO.HIGH)
-   GPIO.output(con2in4,GPIO.LOW)
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("     Moving", 1)
-   lcd.text("    Forward!", 2)
-   
+    global movement_status
+    
+    #front right
+    GPIO.output(con1in1,GPIO.LOW)
+    GPIO.output(con1in2,GPIO.HIGH)
+    
+    #front left
+    GPIO.output(con1in3,GPIO.LOW)
+    GPIO.output(con1in4,GPIO.HIGH)
+    
+    #back left
+    GPIO.output(con2in1,GPIO.LOW)
+    GPIO.output(con2in2,GPIO.HIGH)
+    
+    #back right
+    GPIO.output(con2in3,GPIO.HIGH)
+    GPIO.output(con2in4,GPIO.LOW)
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Moving Forward"
+    
 def backward():
-   GPIO.output(con1in1,GPIO.HIGH)
-   GPIO.output(con1in2,GPIO.LOW)
-   
-   GPIO.output(con1in3,GPIO.HIGH)
-   GPIO.output(con1in4,GPIO.LOW)
-   
-   GPIO.output(con2in1,GPIO.HIGH)
-   GPIO.output(con2in2,GPIO.LOW)
-   
-   GPIO.output(con2in3,GPIO.LOW)
-   GPIO.output(con2in4,GPIO.HIGH)
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("     Moving", 1)
-   lcd.text("    Backward!", 2)
-   
+    global movement_status
+    
+    GPIO.output(con1in1,GPIO.HIGH)
+    GPIO.output(con1in2,GPIO.LOW)
+    
+    GPIO.output(con1in3,GPIO.HIGH)
+    GPIO.output(con1in4,GPIO.LOW)
+    
+    GPIO.output(con2in1,GPIO.HIGH)
+    GPIO.output(con2in2,GPIO.LOW)
+    
+    GPIO.output(con2in3,GPIO.LOW)
+    GPIO.output(con2in4,GPIO.HIGH)
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Moving Backward"
+    
 def strafe_left():
-   GPIO.output(con1in1,GPIO.LOW)
-   GPIO.output(con1in2,GPIO.HIGH)
-  
-   GPIO.output(con1in3,GPIO.HIGH)
-   GPIO.output(con1in4,GPIO.LOW)
+    global movement_status
+    
+    GPIO.output(con1in1,GPIO.LOW)
+    GPIO.output(con1in2,GPIO.HIGH)
    
-   GPIO.output(con2in1,GPIO.LOW)
-   GPIO.output(con2in2,GPIO.HIGH)
-   
-   GPIO.output(con2in3,GPIO.LOW)
-   GPIO.output(con2in4,GPIO.HIGH)
-   
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("     Moving", 1)
-   lcd.text("     Left!", 2)
+    GPIO.output(con1in3,GPIO.HIGH)
+    GPIO.output(con1in4,GPIO.LOW)
+    
+    GPIO.output(con2in1,GPIO.LOW)
+    GPIO.output(con2in2,GPIO.HIGH)
+    
+    GPIO.output(con2in3,GPIO.LOW)
+    GPIO.output(con2in4,GPIO.HIGH)
+    
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Moving Left"
 
 def strafe_right():
-   GPIO.output(con1in1,GPIO.HIGH)
-   GPIO.output(con1in2,GPIO.LOW)
-  
-   GPIO.output(con1in3,GPIO.LOW)
-   GPIO.output(con1in4,GPIO.HIGH)
+    global movement_status
+    
+    GPIO.output(con1in1,GPIO.HIGH)
+    GPIO.output(con1in2,GPIO.LOW)
    
-   GPIO.output(con2in1,GPIO.HIGH)
-   GPIO.output(con2in2,GPIO.LOW)
-   
-   GPIO.output(con2in3,GPIO.HIGH)
-   GPIO.output(con2in4,GPIO.LOW)
-   
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("     Moving", 1)
-   lcd.text("     Right!", 2)
-   
+    GPIO.output(con1in3,GPIO.LOW)
+    GPIO.output(con1in4,GPIO.HIGH)
+    
+    GPIO.output(con2in1,GPIO.HIGH)
+    GPIO.output(con2in2,GPIO.LOW)
+    
+    GPIO.output(con2in3,GPIO.HIGH)
+    GPIO.output(con2in4,GPIO.LOW)
+    
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Moving Right"
+    
 def turn_right():
-   print("Turn right")
-   
-   GPIO.output(con1in1,GPIO.HIGH)
-   GPIO.output(con1in2,GPIO.LOW)
-   
-   GPIO.output(con1in3,GPIO.LOW)
-   GPIO.output(con1in4,GPIO.HIGH)
-   
-   GPIO.output(con2in1,GPIO.LOW)
-   GPIO.output(con2in2,GPIO.HIGH)
-   
-   GPIO.output(con2in3,GPIO.LOW)
-   GPIO.output(con2in4,GPIO.HIGH)
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("    Turning", 1)
-   lcd.text("     Right!", 2)
-   
-   
+    global movement_status
+    
+    GPIO.output(con1in1,GPIO.HIGH)
+    GPIO.output(con1in2,GPIO.LOW)
+    
+    GPIO.output(con1in3,GPIO.LOW)
+    GPIO.output(con1in4,GPIO.HIGH)
+    
+    GPIO.output(con2in1,GPIO.LOW)
+    GPIO.output(con2in2,GPIO.HIGH)
+    
+    GPIO.output(con2in3,GPIO.LOW)
+    GPIO.output(con2in4,GPIO.HIGH)
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Turning Right"
+    
+    
 def turn_left():
-   GPIO.output(con1in1,GPIO.LOW)
-   GPIO.output(con1in2,GPIO.HIGH)
-   
-   GPIO.output(con1in3,GPIO.HIGH)
-   GPIO.output(con1in4,GPIO.LOW)
-   
-   GPIO.output(con2in1,GPIO.HIGH)
-   GPIO.output(con2in2,GPIO.LOW)
-   
-   GPIO.output(con2in3,GPIO.HIGH)
-   GPIO.output(con2in4,GPIO.LOW)
-   
-   
-   front_right_pwm.ChangeDutyCycle(100)
-   front_left_pwm.ChangeDutyCycle(100)
-   back_left_pwm.ChangeDutyCycle(100)
-   back_right_pwm.ChangeDutyCycle(100)
-   
-   lcd.text("    Turning", 1)
-   lcd.text("    Left!", 2)
-   
+    global movement_status
+    
+    GPIO.output(con1in1,GPIO.LOW)
+    GPIO.output(con1in2,GPIO.HIGH)
+    
+    GPIO.output(con1in3,GPIO.HIGH)
+    GPIO.output(con1in4,GPIO.LOW)
+    
+    GPIO.output(con2in1,GPIO.HIGH)
+    GPIO.output(con2in2,GPIO.LOW)
+    
+    GPIO.output(con2in3,GPIO.HIGH)
+    GPIO.output(con2in4,GPIO.LOW)
+    
+    
+    front_right_pwm.ChangeDutyCycle(100)
+    front_left_pwm.ChangeDutyCycle(100)
+    back_left_pwm.ChangeDutyCycle(100)
+    back_right_pwm.ChangeDutyCycle(100)
+    
+    movement_status = "Turning Left"
+    
 def stop():
-   front_right_pwm.ChangeDutyCycle(0)
-   front_left_pwm.ChangeDutyCycle(0)
-   back_left_pwm.ChangeDutyCycle(0)
-   back_right_pwm.ChangeDutyCycle(0)
-   
-   lcd.text("     Hello", 1)
-   lcd.text("     World", 2)
-   
-#device = InputDevice('/dev/input/event1')
-   
+    global movement_status
+    
+    front_right_pwm.ChangeDutyCycle(0)
+    front_left_pwm.ChangeDutyCycle(0)
+    back_left_pwm.ChangeDutyCycle(0)
+    back_right_pwm.ChangeDutyCycle(0)
+    
+    movement_status = "Hello World"
+    
+#device = InputDevice('/dev/input/event3')
+    
 device = InputDevice('/dev/input/event0')
 
-#track state of each directional key
-direction_pressed = {
-   ecodes.KEY_W: False,
-   ecodes.KEY_S: False,
-   ecodes.KEY_A: False,
-   ecodes.KEY_D: False,
-   ecodes.KEY_LEFT: False,
-   ecodes.KEY_RIGHT: False
-}
+def handle_movement():
+    #track state of each directional key
+    direction_pressed = {
+        ecodes.KEY_W: False,
+        ecodes.KEY_S: False,
+        ecodes.KEY_A: False,
+        ecodes.KEY_D: False,
+        ecodes.KEY_LEFT: False,
+        ecodes.KEY_RIGHT: False
+    }
+    
+    for event in device.read_loop():
+        if event.type == ecodes.EV_KEY:
+            if event.code in direction_pressed:
+                if event.value == 1:
+                    direction_pressed[event.code] = True
+                    if any(direction_pressed.values()):
+                        if direction_pressed[ecodes.KEY_W]:
+                            forward()
+                        elif direction_pressed[ecodes.KEY_S]:
+                            backward()
+                        elif direction_pressed[ecodes.KEY_A]:
+                            strafe_left()
+                        elif direction_pressed[ecodes.KEY_D]:
+                            strafe_right()
+                        elif direction_pressed[ecodes.KEY_LEFT]:
+                            turn_left()
+                        elif direction_pressed[ecodes.KEY_RIGHT]:
+                            turn_right()
+                    else:
+                        stop()
+                elif event.value == 0:
+                    direction_pressed[event.code] = False
+                    if not any(direction_pressed.values()):
+                        stop()
 
-#check if any directional key is pressed
-def any_direction_pressed():
-   return any(direction_pressed.values())
+    
+def update_lcd():
+    global movement_status
+    prev_status = ""
+    
+    while True:
+        if movement_status != prev_status:
+            lcd.clear()
+            if movement_status == "Hello World":
+                lcd.write_string("     Hello\n\r     World!")
+            elif movement_status == "Moving Forward":
+                lcd.write_string("     Moving\n\r    Forward!")
+            elif movement_status == "Moving Backward":
+                lcd.write_string("     Moving\n\r    Backward!")
+            elif movement_status == "Moving Left":
+                lcd.write_string("     Moving\n\r     Left!")
+            elif movement_status == "Moving Right":
+                lcd.write_string("     Moving\n\r     Right!")
+            elif movement_status == "Turning Left":
+                lcd.write_string("    Turning\n\r     Left!")
+            elif movement_status == "Turning Right":
+                lcd.write_string("    Turning\n\r    Right!")
+            prev_status = movement_status
+        
+        
+movement_thread = threading.Thread(target=handle_movement)
+movement_thread.start()
 
-lcd.text("     Hello", 1)
-lcd.text("     World", 2)
 
-for event in device.read_loop():
-   if event.type == ecodes.EV_KEY:
-       if event.code in direction_pressed:
-           if event.value == 1:
-               direction_pressed[event.code] = True
-               if any_direction_pressed():
-                   if direction_pressed[ecodes.KEY_W]:
-                       forward()
-                   elif direction_pressed[ecodes.KEY_S]:
-                       backward()
-                   elif direction_pressed[ecodes.KEY_A]:
-                       strafe_left()
-                   elif direction_pressed[ecodes.KEY_D]:
-                       strafe_right()
-                   elif direction_pressed[ecodes.KEY_LEFT]:
-                       turn_left()
-                   elif direction_pressed[ecodes.KEY_RIGHT]:
-                       turn_right()
-               else:
-                   stop()
-           elif event.value == 0:
-               direction_pressed[event.code] = False
-               if not any_direction_pressed():
-                   stop()
+lcd_thread = threading.Thread(target=update_lcd)
+lcd_thread.start()
+
+# make sure threads finish before cleanup
+movement_thread.join()
+lcd_thread.join()
 
 GPIO.cleanup()
